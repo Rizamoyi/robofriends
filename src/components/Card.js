c150c1e111da87bd643ac1316ec6fd82c9630915
rizamoyi <samwelmoyi@gmail.com>
2021-11-04 16:13:25 +0300

    update code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,11 @@
-const Card = ({ id, name, email }) => {
-  return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-2">
-      <img src={`https://robohash.org/${id}`} alt="robots" />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
+const Card = ({ id, name, email }) => (
+  <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-2">
+    <img src={`https://robohash.org/${id}`} alt="robots" />
+    <div>
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
