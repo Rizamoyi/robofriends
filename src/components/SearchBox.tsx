f64d9ca7853e4825b717075134db7d46499ac5ab
rizamoyi <samwelmoyi@gmail.com>
2022-10-27 15:59:23 +0300

    typescript setup

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,10 @@
-const SearchBox = ({ searchChange }) => (
+import { ChangeEvent } from 'react';
+
+type SearchBoxProps = {
+  searchChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SearchBox = ({ searchChange }: SearchBoxProps) => (
   <div className="pa2">
     <input
       className="pa3 ba b--green bg-lightest-blue"
